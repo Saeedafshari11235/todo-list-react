1d81654306fd1ff3f163523a56d7fa672767be86
SaeedAfshari11235 <msafshari11235@gmail.com>
2025-03-31 20:00:32 +0330

    Add inputContainer component

---
+++ src/component/InputContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './InputContainer.css'
+
+export default class InputContainer extends Component {
+  render() {
+    return (
+      <div className='input-container'>
+        
+      </div>
+    )
+  }
+}
--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
+import InputContainer from './InputContainer'
+import './TodoList.css'
 
 export default class TodoList extends Component {
   render() {
     return (
-      <div>TodoList</div>
+      <div className='todo-list'>
+        <h1>Todo List</h1>
+        <InputContainer></InputContainer>
+      </div>
     )
   }
 }
