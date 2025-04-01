69de3bf7b5f48638bbbe750f9f940c1c13db2084
SaeedAfshari11235 <msafshari11235@gmail.com>
2025-04-01 09:37:50 +0330

    add text input and add button to the component

--- src/component/InputContainer.js
+++ src/component/InputContainer.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
+import { IoMdAdd } from "react-icons/io";
 import './InputContainer.css'
 
 export default class InputContainer extends Component {
   render() {
     return (
       <div className='input-container'>
-        
+        <input type='text'/>
+        <button className='add-todo'><IoMdAdd size={17} color='white'/></button>
       </div>
     )
   }
