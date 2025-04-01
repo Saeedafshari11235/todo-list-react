289a0cffff36a5b72d579c96f18f6a910bbf73f9
SaeedAfshari11235 <msafshari11235@gmail.com>
2025-04-01 10:01:04 +0330

    Add Todo proccess

--- src/component/InputContainer.js
+++ src/component/InputContainer.js
@@ -6,8 +6,8 @@ export default class InputContainer extends Component {
   render() {
     return (
       <div className='input-container'>
-        <input type='text' placeholder='Add your Todos here'/>
-        <button className='add-todo'><IoMdAdd size={17} color='white'/></button>
+        <input type='text' placeholder='Add your Todos here' onChange={this.props.setTodoValue}/>
+        <button className='add-todo' onClick={this.props.addTodo}><IoMdAdd size={17} color='white'/></button>
       </div>
     )
   }
--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -9,13 +9,31 @@ export default class TodoList extends Component {
       todoValue: "",
       todoList: [],
     };
+    this.setTodoValue = this.setTodoValue.bind(this);
+    this.addTodo = this.addTodo.bind(this);
+  }
+  setTodoValue(event) {
+    this.setState({ todoValue: event.target.value });
+  }
+  addTodo() {
+    let todoList = [];
+    todoList.push({ text: this.state.todoValue, completed: false });
+    this.setState({ todoList: todoList });
+    console.log(todoList);
   }
   render() {
     return (
       <div className="todo-list">
         <h1>Todo List</h1>
-        <InputContainer></InputContainer>
-        <div className="todo-container"></div>
+        <InputContainer
+          setTodoValue={this.setTodoValue}
+          addTodo={this.addTodo}
+        ></InputContainer>
+        <div className="todo-container">
+            {this.state.todoList.map((item)=>{
+                return(<div>{item.text}</div>)
+            })}
+        </div>
       </div>
     );
   }
