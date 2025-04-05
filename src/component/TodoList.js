29366562624e2e58e8401cf81bce2aa9bd7c5824
SaeedAfshari11235 <msafshari11235@gmail.com>
2025-04-05 19:24:11 +0330

    Refactor code using hooks

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,60 +1,33 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import InputContainer from "./InputContainer";
 import "./TodoList.css";
 import Todo from "./Todo";
 
-export default class TodoList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todoValue: "",
-      todoList: [],
-    };
-    this.setTodoValue = this.setTodoValue.bind(this);
-    this.addTodo = this.addTodo.bind(this);
-    this.removeTodo = this.removeTodo.bind(this);
-  }
+export default function TodoList() {
+  const [todoValue, setTodoValue] = useState("");
+  const [todoList, setTodoList] = useState([]);
 
-  setTodoValue(event) {
-    this.setState({ todoValue: event.target.value });
-  }
+  const addTodo = () => {
+    let todoArray = todoList;
+    todoArray.push({ text: todoValue });
+    setTodoList(todoArray);
+  };
 
-  addTodo() {
-    let todoList = this.state.todoList;
-    todoList.push({ text: this.state.todoValue, completed: false });
-    this.setState((prev) => {
-      return {
-        ...prev,
-        todoList: todoList,
-      };
-    });
-  }
-
-  removeTodo(text) {
-    this.setState((prev) => {
-      return {
-        todoList: prev.todoList.filter((item) => item.text !== text),
-      };
-    });
-  }
-
-  render() {
-    return (
-      <div className="todo-list">
-        <h1>Todo List</h1>
-        <InputContainer
-          setTodoValue={this.setTodoValue}
-          addTodo={this.addTodo}
-        ></InputContainer>
-        <div className="todo-container">
-          {this.state.todoList.map((item) => (
-            <Todo
-              {...item}
-              removeTodo={() => this.removeTodo(item.text)}
-            ></Todo>
-          ))}
-        </div>
+  const removeTodo = (text) => {
+    setTodoList(todoList.filter((item) => item.text !== text));
+  };
+  return (
+    <div className="todo-list">
+      <h1>Todo List</h1>
+      <InputContainer
+        setTodoValue={(event) => setTodoValue(event.target.value)}
+        addTodo={() => addTodo()}
+      ></InputContainer>
+      <div className="todo-container">
+        {todoList.map((item) => (
+          <Todo {...item} removeTodo={() => removeTodo(item.text)}></Todo>
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
 }
